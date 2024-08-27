eb8b152a879366433570a49a38bd7b69ec859e19
Simin Li <siminli94@gmail.com>
2024-08-26 22:27:14 -0500

    Update TwoSum1.java

--- TwoSum1.java
+++ TwoSum1.java
@@ -35,7 +35,7 @@ import java.util.Map;
 public class TwoSum1 {
         public int[] twoSum(int[] nums, int target) {
             // create a map to store num as key and index as value
-            Map<Integer, Integer> map = new HashMap<>();
+            HashMap<Integer, Integer> map = new HashMap<>();
             // loop through the integer array, check if the difference existed in the map, return the index
             // otherwise, add num key and index as value to the map
             for (int i = 0; i < nums.length; i++){
@@ -47,6 +47,68 @@ public class TwoSum1 {
                 map.put(nums[i], i);
             }
 
-            return null;
+            return new int[]{};
         }
+}
+
+/* ## KEY POINT ##
+One object is used as a key (index) to another object (value). 
+It can store different types: String keys and Integer values, or the same type, like: String keys and String values:
+
+HashMap<String, String> map = new HashMap<String, String>();
+- Add an item: map.put("String", "String")
+- Access an item: map.get("key")
+- Remove an item: map.remove("key")
+- Remove all items: map.clear()
+- Get the size of HashMap: map.size()
+- Loop through the HashMap -- Use the keySet() method if you only want the keys,
+        and use the values() method if you only want the values:
+// Print keys
+for (String i : capitalCities.keySet()) {
+  System.out.println(i);
+} 
+
+// Print values
+for (String i : capitalCities.values()) {
+  System.out.println(i);
+}
+
+*/
+
+
+
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> newMap = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            int difference = target - num;
+
+            // if the diff in the map, return the indexes
+            if (newMap.containsKey(difference)){
+                // put newMap.get(difference) at first as putting the smaller index first
+                // newMap.get(difference) returns the index of difference and i is the index of the num
+                return new int[]{newMap.get(difference), i};
+            }
+            newMap.put(num, i);
+        }
+        return new int[] {};
+
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length - 1; i ++){
+            for ( int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
 }

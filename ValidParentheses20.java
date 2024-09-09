357537fc4ea44493954a3d153d6202d0b2e09316
Simin Li <siminli94@gmail.com>
2024-09-08 22:55:04 -0500

    Update ValidParentheses20.java

--- ValidParentheses20.java
+++ ValidParentheses20.java
@@ -29,6 +29,16 @@ Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
  */
+
+
+/*
+stack: LIFO last in first out -- the last element added to the stack will be the first one to be removed.
+stack.empty() -- It returns true if nothing is on the top of the stack. Else, returns false.
+stack.peek() -- Returns the element on the top of the stack, but does not remove it.
+stack.pop() -- Removes and returns the top element of the stack. An ‘EmptyStackException’. An exception is thrown if we call pop() when the invoking stack is empty.
+stack.push() -- Add an element on the top of the stack
+stack.search() -- It determines whether an object exists in the stack. If the element is found. It returns the position of the element from the top of the stack. Else, it returns -1.
+*/
 class Solution20 {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
@@ -41,13 +51,13 @@ class Solution20 {
             // if not, check if the closisng bracket is empty, return false
             // ")"
             else {
-                // the character is a closing bracket without matching bracket, return false
+                // the character is a closing bracket without a matching bracket, return false
                 if(stack.isEmpty()){
                     return false;
                 }
 
-                // there are matching characters in the string,
-                char lastChar = stack.peek();
+                // remove the open and closed sign if they matched
+                char lastChar = stack.peek(); // Last in first out to get the first closing sign
                 if((c == ')' && lastChar == '(')|| (c == '}' && lastChar == '{') || (c == ']' && lastChar == '[')){
                     stack.pop();
                 }
@@ -63,3 +73,62 @@ class Solution20 {
 
     }
 }
+
+
+// ([]) -- stack: last in first out, stack.pop() can get the fist top object of the stack
+// ], } -- cheeck if stack is empty
+class Solution {
+    public boolean isValid(String s) {
+         Stack<Character> stack = new Stack<>();
+
+        for (char x : s.toCharArray()
+             ) {
+
+            if(x=='('){
+                
+                stack.push(')');
+            } else if (x=='{') {
+                stack.push('}');
+                
+            } else if (x=='[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop()!=x) {
+
+                return false;
+            }
+
+
+        }
+
+
+        return stack.isEmpty();
+        
+    }
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> brackets = new Stack<>();
+        Map<Character, Character> bracketLookup = new HashMap<>(3);
+
+        bracketLookup.put(')', '(');
+        bracketLookup.put('}', '{');
+        bracketLookup.put(']', '[');
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (bracketLookup.containsKey(c)) {
+                if (!brackets.isEmpty() && bracketLookup.get(c).equals(brackets.peek())) {
+                    brackets.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                brackets.push(c);
+            }
+        }
+
+        return brackets.isEmpty();
+    }
+}
+

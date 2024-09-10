2da766e3d3db90b436356ce4158de7913076935b
Simin Li <siminli94@gmail.com>
2024-09-09 22:57:26 -0500

    Update ReverseLinkedList06.java

--- ReverseLinkedList06.java
+++ ReverseLinkedList06.java
@@ -38,9 +38,11 @@ The number of nodes in the list is the range [0, 5000].
 
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode current = head;
-        ListNode prev = null;
-        ListNode nextCurrentV = null;
+
+     // using a singly linkedlist, it only has a next pointer for each node
+        ListNode current = head;  // points to the node currently being processed
+        ListNode prev = null; // track the previous node
+        ListNode nextCurrentV = null; // A temp pointer to store current.next
 
         /* 
         head = [ 0 ,    1,    2,   3]
@@ -48,17 +50,50 @@ class Solution {
 origin: prev ->current -> next
 return: null <- 0 <- 1 <- 2 <- 3 <- head
         prev <- current <- next
+
+        For a linked list like [0 -> 1 -> 2 -> 3 -> null]:
+        Before Reversal: head -> 0 -> 1 -> 2 -> 3 -> null
+        prev = null; current = 1; nextCurrentV = 2.
+
+        First iteration:
+        current.next = prev: 0's next points to null
+        pre = current: move prev to current -- prev = 0
+        current = nextCurrentV: current = 1
+        null <- 0       1 -> 2 -> 3 -> null
+      
+        second iteration:
+        current.next = prev: 1's next points to 0
+        pre = current: move prev to current -- prev = 1
+        current = nextCurrentV: current = 2
+        null <- 0 <- 1      2 -> 3 -> null
+        
+        third iteration:
+        current.next = prev: 2's next points to 1
+        pre = current: move prev to current -- prev = 2
+        current = nextCurrentV: current = 3
+        null <- 0 <- 1 <- 2     3 -> null
+
+        fourth iteration:
+        current.next = prev: 3's next points to 2
+        pre = current: move prev to current -- prev = 3
+        current = nextCurrentV: current = null
+        null <- 0 <- 1 <- 2 <- 3  
+
+        now current = null, prev points to 3, which is the new head of the reversed list 
+        
+        After Reversal: null <- 0 <- 1 <- 2 <- 3 <- head
+        
         */
 
-        // current reached null, that meand the end of the listNode
+        // current reached null, that means we reach the end of the list
         while(current != null) {
             // assign the pointer of current next at first, then we need to shift current
             nextCurrentV = current.next; 
-            current.next = prev;
-            prev = current;
+            current.next = prev; // reverse the next pointer, making the link point backward
+            prev = current; 
             current = nextCurrentV;
         }
-        return prev;
+        return prev;  // Returning prev gives the new head of the reversed linked list.
     }
 }
 
